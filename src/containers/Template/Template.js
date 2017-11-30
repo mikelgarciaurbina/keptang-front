132956e35fcb07b6c22d409a9ba00d5108cf0374
Mikel Garcia <mikel_6@hotmail.com>
2017-11-30 13:15:08 +0100

    Add eslint airbnb

--- src/containers/Template/Template.js
+++ src/containers/Template/Template.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Template.css';
 
-class Template extends Component {
-  render() {
-    return (
-      <div className="Template">
-        <div className="Template-document" />
-      </div>
-    );
-  }
-}
+const Template = () => (
+  <div className="Template">
+    <div className="Template-document" />
+  </div>
+);
 
 export default Template;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import { Template } from './containers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Template />, document.getElementById('root'));
+ReactDOM.render(<Template />, document.getElementById('root')); // eslint-disable-line no-undef
 registerServiceWorker();
