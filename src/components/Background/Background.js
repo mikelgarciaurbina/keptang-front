c473da7ad8cc12d91ee1b974e502237859ed9034
Mikel Garcia <mikel_6@hotmail.com>
2017-11-30 13:46:45 +0100

    Add background component

---
+++ src/components/Background/Background.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+
+import './Background.css';
+
+class Background extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: {
+        x: 0,
+        y: 0,
+      },
+    };
+
+    this.onDragStop = this.onDragStop.bind(this);
+  }
+
+  onDragStop(e, position) {
+    const { x, y } = position;
+    this.setState({ position: { x, y } });
+  }
+
+  render() {
+    const { position } = this.state;
+
+    return (
+      <Draggable bounds="parent" position={position} handle="strong" onStop={this.onDragStop}>
+        <div className="box no-cursor">
+          <strong className="cursor"><div>Drag here</div></strong>
+        </div>
+      </Draggable>
+    );
+  }
+}
+
+export default Background;
--- src/containers/Template/Template.js
+++ src/containers/Template/Template.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
+import { Background } from '../../components';
 import './Template.css';
 
 const Template = () => (
   <div className="Template">
-    <div className="Template-document" />
+    <div className="Template-document">
+      <Background />
+    </div>
   </div>
 );
 
