d18a07fe0da3d62b136e71702d70c8307e19ffe0
Mikel Garcia <mikel_6@hotmail.com>
2017-12-01 04:36:01 +0100

    Add icon and show only on hover

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 
+import dragIcon from '../../assets/icons/ic_open_with_black_24px.svg';
 import './Background.css';
 
 class Background extends Component {
@@ -8,34 +9,57 @@ class Background extends Component {
     super(props);
 
     this.state = {
+      dragging: false,
+      hover: false,
       position: {
         x: 0,
         y: 0,
       },
     };
 
+    this.onDrag = this.onDrag.bind(this);
     this.onDragStop = this.onDragStop.bind(this);
+    this.onHover = this.onHover.bind(this);
+  }
+
+  onDrag() {
+    const { dragging } = this.state;
+    if (!dragging) this.setState({ dragging: true });
   }
 
   onDragStop(e, position) {
     const { x, y } = position;
-    this.setState({ position: { x, y } });
+    this.setState({ dragging: false, position: { x, y } });
+  }
+
+  onHover() {
+    this.setState(({ hover }) => ({ hover: !hover }));
   }
 
   render() {
-    const { position } = this.state;
+    const { dragging, hover, position } = this.state;
 
     return (
       <Draggable
         bounds="parent"
         position={position}
         handle="strong"
+        onDrag={this.onDrag}
         onStop={this.onDragStop}
       >
-        <div className="Background-box Background-no-cursor">
-          <strong className="Background-cursor">
-            <div>Drag here</div>
-          </strong>
+        <div
+          onMouseEnter={this.onHover}
+          onMouseLeave={this.onHover}
+          className="Background-box Background-no-cursor"
+        >
+          { (dragging || hover) && (
+            <strong className="Background-cursor">
+              <div
+                className="Background-icon"
+                style={{ backgroundImage: `url(${dragIcon})` }}
+              />
+            </strong>
+          )}
         </div>
       </Draggable>
     );
