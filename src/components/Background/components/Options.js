1be38310e870f1c497f1531bb9b9e56f40a47242
Mikel Garcia <mikel_6@hotmail.com>
2017-12-07 10:52:13 +0100

    Fix options div in Background component

--- src/components/Background/components/Options.js
+++ src/components/Background/components/Options.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 const Options = styled.div`
-  right: 10px;
-  position: relative;
-  top: ${props => (`${props.dimension.height + 16}px`)};
+  left: 0px;
+  position: absolute;
+  top: ${props => (`${props.dimension.height + 26}px`)};
 `;
 Options.defaultProps = {
   dimension: {
