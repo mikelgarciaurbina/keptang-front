c043e514cf24dceb633f88cd09db72c644d5d0af
Mikel Garcia <mikel_6@hotmail.com>
2018-01-01 03:19:31 +0100

    Add Resizable atoms

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -2,5 +2,6 @@ export Icon from './Icon';
 export LeftIcon from './LeftIcon';
 export OptionsContainer from './OptionsContainer';
 export P from './Paragraph';
+export Resizable from './Resizable';
 export RightIcon from './RightIcon';
 export Strong from './Strong';
