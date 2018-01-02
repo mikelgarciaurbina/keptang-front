33a7962b9358e965dacfe5cb7f0de14fa607695f
Mikel Garcia <mikel_6@hotmail.com>
2018-01-02 06:32:18 +0100

    Refactor Resizable atom

--- src/atoms/Resizable/Resizable.js
+++ src/atoms/Resizable/Resizable.js
@@ -5,19 +5,19 @@ const Resizable = styled(Resiz)`
   position: absolute;
 
   & > span {
-    visibility: ${({ hover }) => !hover && 'hidden'};
-    position: absolute;
-    width: 20px;
-    height: 20px;
-    bottom: 0;
-    right: 0;
     background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2IDYiIHN0eWxlPSJiYWNrZ3JvdW5kLWNvbG9yOiNmZmZmZmYwMCIgeD0iMHB4IiB5PSIwcHgiIHdpZHRoPSI2cHgiIGhlaWdodD0iNnB4Ij48ZyBvcGFjaXR5PSIwLjMwMiI+PHBhdGggZD0iTSA2IDYgTCAwIDYgTCAwIDQuMiBMIDQgNC4yIEwgNC4yIDQuMiBMIDQuMiAwIEwgNiAwIEwgNiA2IEwgNiA2IFoiIGZpbGw9IiMwMDAwMDAiLz48L2c+PC9zdmc+');
-    background-position: bottom right;
-    padding: 0 3px 3px 0;
-    background-repeat: no-repeat;
     background-origin: content-box;
+    background-position: bottom right;
+    background-repeat: no-repeat;
+    bottom: 0;
     box-sizing: border-box;
     cursor: se-resize;
+    height: 20px;
+    padding: 0 3px 3px 0;
+    position: absolute;
+    right: 0;
+    visibility: ${({ hover }) => !hover && 'hidden'};
+    width: 20px;
   }
 `;
 Resizable.defaultProps = {
