c2e514635761d4584a941ca8209607f7590e0abb
Mikel Garcia <mikel_6@hotmail.com>
2018-01-06 05:05:55 +0100

    Alphabetic order in Background/Container

--- src/components/Background/atoms/Container.js
+++ src/components/Background/atoms/Container.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  background: ${({ background }) => background};
   cursor: auto;
+  display: inline-block;
   height: ${({ dimension: { height } }) => `${height}px`};
   margin: 10px;
   padding: 10px;
   width: ${({ dimension: { width } }) => `${width}px`};
-  background: ${({ background }) => background};
-  display: inline-block;
 `;
 Container.defaultProps = {
   background: '#1273de',
