ba6d4cc3c9b16c77e955342be3256b8d624f6957
Mikel Garcia <mikel_6@hotmail.com>
2018-01-14 11:06:31 +0700

    Fix url in DraggableIcon molecules

--- src/molecules/DraggableIcon.js
+++ src/molecules/DraggableIcon.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Color from 'color';
 
-import { LeftIcon, Strong } from '../../atoms';
+import { LeftIcon, Strong } from '../atoms';
 
-import dragBlackIcon from '../../assets/icons/ic_open_with_black_24px.svg';
-import dragWhiteIcon from '../../assets/icons/ic_open_with_white_24px.svg';
+import dragBlackIcon from '../assets/icons/ic_open_with_black_24px.svg';
+import dragWhiteIcon from '../assets/icons/ic_open_with_white_24px.svg';
 
 const DraggableIcon = ({ color }) => (
   <Strong>
