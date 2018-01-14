597d4362e7aa7486e4070b9cc0d8dd479842cced
Mikel Garcia <mikel_6@hotmail.com>
2018-01-14 11:07:03 +0700

    Fix url in OptionsIcon molecules

--- src/molecules/OptionsIcon.js
+++ src/molecules/OptionsIcon.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Color from 'color';
 
-import { RightIcon } from '../../atoms';
+import { RightIcon } from '../atoms';
 
-import optionsBlackIcon from '../../assets/icons/ic_settings_black_24px.svg';
-import optionsWhiteIcon from '../../assets/icons/ic_settings_white_24px.svg';
+import optionsBlackIcon from '../assets/icons/ic_settings_black_24px.svg';
+import optionsWhiteIcon from '../assets/icons/ic_settings_white_24px.svg';
 
 const OptionsIcon = ({ color, ...props }) => (
   <RightIcon image={Color(color).light() ? optionsBlackIcon : optionsWhiteIcon} {...props} />
