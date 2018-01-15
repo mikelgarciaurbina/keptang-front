66a05357d1cc6ce4344404bc0af463148c42f910
Mikel Garcia <mikel_6@hotmail.com>
2018-01-15 16:30:32 +0700

    Add border option to Column

--- src/components/Concepts/atoms/Column.js
+++ src/components/Concepts/atoms/Column.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import Color from 'color';
 
 const Column = styled.div`
   display: flex;
   flex-direction: column;
 
   flex: ${({ flex }) => flex};
+
+  ${({ border }) => border && `
+    border-bottom-style: double;
+    border-bottom-color: ${Color('#000000').alpha(0.2).string()};
+    padding-bottom: 7px;
+  `}
 `;
 Column.defaultProps = {
   flex: 1,
