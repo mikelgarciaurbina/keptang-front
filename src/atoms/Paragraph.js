2ab4d522224db97ae69e76e73422f73a84ae9f4a
Mikel Garcia <mikel_6@hotmail.com>
2018-01-22 16:04:34 +0700

    Add contants in Paragraph

--- src/atoms/Paragraph.js
+++ src/atoms/Paragraph.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
+import { THEME } from '../constants';
+
+const { UNIT } = THEME;
 const Paragraph = styled.p`
-  margin-bottom: 1px;
-  margin-top: 10px;
+  margin-bottom: ${UNIT * 0.1}px;
+  margin-top: ${UNIT}px;
   width: 100%;
 
   ${({ flex }) => flex && `flex: ${flex};`};
