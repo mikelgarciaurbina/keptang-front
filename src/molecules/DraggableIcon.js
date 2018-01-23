2379777b176c7d5b9542b4b5759d466026f6c4f8
Mikel Garcia <mikel_6@hotmail.com>
2018-01-23 12:36:31 +0700

    Add contants in DraggableIcon

--- src/molecules/DraggableIcon.js
+++ src/molecules/DraggableIcon.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Color from 'color';
 
+import { THEME } from '../constants';
 import { LeftIcon, Strong } from '../atoms';
+import { ProviderColor } from '../providers';
 
 import dragBlackIcon from '../assets/icons/ic_open_with_black_24px.svg';
 import dragWhiteIcon from '../assets/icons/ic_open_with_white_24px.svg';
 
 const DraggableIcon = ({ color }) => (
   <Strong>
-    <LeftIcon image={Color(color).light() ? dragBlackIcon : dragWhiteIcon} />
+    <LeftIcon image={ProviderColor.light(color) ? dragBlackIcon : dragWhiteIcon} />
   </Strong>
 );
 DraggableIcon.propTypes = {
   color: PropTypes.string,
 };
 DraggableIcon.defaultProps = {
-  color: '#FFFFFF',
+  color: THEME.WHITE,
 };
 
 export default DraggableIcon;
