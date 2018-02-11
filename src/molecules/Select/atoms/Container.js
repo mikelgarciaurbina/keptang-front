06e1d4f19844f621accea8c12c8cbdaaea564499
Mikel Garcia <mikel_6@hotmail.com>
2018-02-11 17:07:45 +0700

    Refactor Container atom in Select molecule

--- src/molecules/Select/atoms/Container.js
+++ src/molecules/Select/atoms/Container.js
@@ -1,19 +1,22 @@
 import styled from 'styled-components';
 
+import { THEME } from '../../../constants';
+
+const { BLACK, TRANSPARENT, UNIT } = THEME;
 const Container = styled.div`
   position: relative;
 
   &:after {
-    border-left: 4px solid transparent;
-    border-right: 4px solid transparent;
-    border-top: 6px solid #000;
+    border-left: ${UNIT * 0.4}px solid ${TRANSPARENT};
+    border-right: ${UNIT * 0.4}px solid ${TRANSPARENT};
+    border-top: ${UNIT * 0.6}px solid ${BLACK};
     content: '';
     height: 0;
     padding: 0;
     pointer-events: none;
     position: absolute;
-    right: 8px;
-    top: 12px;
+    right: ${UNIT * 0.8}px;
+    top: ${UNIT * 1.2}px;
     width: 0;
   }
 `;
